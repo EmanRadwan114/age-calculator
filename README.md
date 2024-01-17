01a0aa1e78b3efa99a8befdb01e65301ef19cc41
EmanRadwan114 <emanalaa11499@gmail.com>
2024-01-17 02:07:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Frontend Mentor - Age Calculator solution
+
+This is a solution to the [Age Calculator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).
+
+### The challenge Description
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The date is in the future
+  - The date is invalid, e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- See the age numbers animate to their final number when the form is submitted
+
+### Links
+
+- [Solution Link](https://your-solution-url.com)
+- [Live Demo Link](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+### Steps I followed
+
+- Build the UI using HTML & CSS
+- Start working with JavaScript as follows:
+  1. check for values validation on value change
+  2. show error msgs if there is any invalid input value
+  3. get user values when he clicks on submit btn
+  4. calculate user age
+  5. check for the validity of these values on submit
+  6. display user age
---
+++ js/main.js
@@ -0,0 +1,169 @@
+/*
+steps:
+  1. check for values validation on value change
+  2. show error msgs if there is any invalid input value
+  3. get user values when he clicks on submit btn
+  4. calculate user age
+  5. check for the validity of these values on submit
+  6. display user age
+*/
+
+// * prepare the project with some declarations & definitions
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const userDay = document.querySelector(".ageCalculation .days");
+const userMonth = document.querySelector(".ageCalculation .months");
+const userYear = document.querySelector(".ageCalculation .year");
+const form = document.querySelector("form");
+const InputFields = document.querySelectorAll("input[type='number'");
+
+const validValues = {
+  day: /^([0-2][0-9]|3[0-1])$/, //from 1 - 31 in 2 digits (eg: 01)
+  month: /^(0[0-9]|1[[0-2])$/, //from 1 - 12 in 2 digits (eg: 01)
+  year: new Date().getFullYear(), // get current year
+};
+const months30D = [4, 6, 9, 11];
+const months31D = [1, 3, 5, 7, 8, 10, 12];
+let monthDays = 31;
+
+function styleValidity(input, color) {
+  input.style.outline = `1px solid var(--${color})`;
+  input.previousElementSibling.style.color = `var(--${color})`;
+}
+
+function validateDay() {
+  return validValues.day.test(day.value);
+}
+function validateMonth() {
+  return validValues.month.test(month.value);
+}
+
+function validateYear() {
+  return year.value <= validValues.year;
+}
+
+// &prevent user from entering wrong date (eg: 31/04/2005), April has 30 days only
+function validateDayNo(arr, isLeap, userBirthDate) {
+  if (month.value != 2) {
+    // * when user type 31 days in a 30day month, the browser automatically increase the month by one,
+    //* that's why I checked here for userBirthDate.getMonth() not userBirthDate.getMonth() + 1
+    for (let i = 0; i < arr.length; i++) {
+      if (month.value == arr[i] && day.value == 31) {
+        day.nextElementSibling.textContent = "this month has 30 days only";
+        return false;
+      }
+    }
+  } else {
+    if (isLeap && day.value > 29) {
+      day.nextElementSibling.textContent =
+        "this is a leap year, enter a day between 1 - 29";
+      return false;
+    } else if (!isLeap && day.value >= 29) {
+      day.nextElementSibling.textContent =
+        "this is not a leap year, enter a day between 1 - 28";
+      return false;
+    }
+  }
+  return true;
+}
+
+// * step 1 & 2
+InputFields.forEach((input, indx) => {
+  input.addEventListener("change", function (e) {
+    input.nextElementSibling.textContent = ``;
+    styleValidity(e.target, "smokeyGrey");
+
+    if (indx == 0) {
+      if (!validateDay()) {
+        e.target.nextElementSibling.textContent = `Must be a valid day`;
+        styleValidity(e.target, "lightRed");
+      }
+    } else if (indx == 1) {
+      if (!validateMonth()) {
+        e.target.nextElementSibling.textContent = `Must be a valid month`;
+        styleValidity(e.target, "lightRed");
+      }
+    } else if (indx == 2) {
+      if (!validateYear()) {
+        e.target.nextElementSibling.textContent = `Must be a valid year`;
+        styleValidity(e.target, "lightRed");
+      }
+    }
+
+    // &prevent empty data
+    if (e.target.value == "") {
+      e.target.nextElementSibling.textContent = `${e.target.id} can't be empty`;
+      styleValidity(e.target, "lightRed");
+    }
+  });
+});
+
+// *step 3, 4 , 5 & 6
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  userDay.textContent = "- -";
+  userMonth.textContent = "- -";
+  userYear.textContent = "- -";
+
+  //   & step 4
+  const currentDate = new Date();
+  const userBirthDate = new Date(`${year.value}-${month.value}-${day.value}`);
+  const isLeapYear =
+    currentDate.getFullYear() % 4 === 0
+      ? currentDate.getFullYear() % 100 === 0
+      : currentDate.getFullYear() % 400 === 0;
+  let calculatedMonths, calculatedDays, calculatedYears;
+
+  //   ^calculate days
+  function calculateDays() {
+    if (currentDate.getDate() > userBirthDate.getDate()) {
+      calculatedDays = currentDate.getDate() - userBirthDate.getDate();
+    } else if (currentDate.getDate() < userBirthDate.getDate()) {
+      calculatedDays = userBirthDate.getDate() - currentDate.getDate();
+    } else if (currentDate.getDate() == userBirthDate.getDate()) {
+      calculatedDays = 0;
+    }
+  }
+  // ^calculate month
+
+  function calculateMonths() {
+    if (currentDate.getMonth() > userBirthDate.getMonth()) {
+      calculatedMonths =
+        11 - (currentDate.getMonth() - userBirthDate.getMonth()) + 1;
+    } else if (currentDate.getMonth() < userBirthDate.getMonth()) {
+      calculatedMonths =
+        11 - (userBirthDate.getMonth() - currentDate.getMonth()) + 1;
+    } else if (currentDate.getMonth() == userBirthDate.getMonth()) {
+      calculatedMonths = 0;
+    }
+  }
+
+  // ^ calculate year
+
+  function calculateYear() {
+    if (currentDate.getMonth() !== userBirthDate.getMonth()) {
+      calculatedYears =
+        currentDate.getFullYear() - userBirthDate.getFullYear() - 1;
+    } else {
+      calculatedYears = currentDate.getFullYear() - userBirthDate.getFullYear();
+    }
+  }
+
+  //   &steps 5 & 6
+  if (day.value && month.value && year.value) {
+    if (
+      validateDay() &&
+      validateMonth() &&
+      validateYear() &&
+      validateDayNo(months30D, isLeapYear, userBirthDate)
+    ) {
+      calculateDays();
+      calculateMonths();
+      calculateYear();
+      userDay.textContent = calculatedDays;
+      userMonth.textContent = calculatedMonths;
+      userYear.textContent = calculatedYears;
+    }
+  }
+});
